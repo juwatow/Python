bbe0d07ebb7b99f74d9c4314172f3ec2551fbe9f
Jeanne <jeanneu@xello.world>
2019-10-20 19:14:04 -0400

    Add SeelectionSort & Refactoring

--- binary-search.py
+++ binary-search.py
@@ -1,21 +1,21 @@
-def binary_search(orderedList, searchItem):
-    # lowIndex and highIndex: keep track of which part of the ordered list to search
-    lowIndex = 0
-    highIndex = len(orderedList) - 1
+def binary_search(sorted_list, search_item):
+    # low_index and high_index: keep track of which part of the ordered list to search
+    low_index = 0
+    high_index = len(sorted_list) - 1
 
-    while lowIndex <= highIndex: # while we haven't narrowed to 1 element
-        midIndex = int((lowIndex + highIndex) / 2)
-        guess = orderedList[midIndex]
+    while low_index <= high_index: # while we haven't narrowed to 1 element
+        mid_index = int((low_index + high_index) / 2)
+        guess = sorted_list[mid_index]
         
-        if guess == searchItem: # search item has been found
-            return midIndex
-        if guess > searchItem: # guess was too high => focus the search in the lower part of the list
-            highIndex = midIndex - 1
+        if guess == search_item: # search item has been found
+            return mid_index
+        if guess > search_item: # guess was too high => focus the search in the lower part of the list
+            high_index = mid_index - 1
         else: # guess was too small => focus the search in the upper part of the list
-            lowIndex = midIndex + 1
+            low_index = mid_index + 1
     return None # search item is not in the list
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-print(binary_search(my_list, 10)) #case: search term in the list
+print(binary_search(my_list, 9)) #case: search term in the list
 print(binary_search(my_list, 20)) #case: search term not in the list
